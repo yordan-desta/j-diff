6901101285c10782f3b4aa1fdc688977a7d18042
Yordanos Desta <yordan.dev@gmail.com>
2017-05-10 15:35:54 +0300

    IDifferentiable interface created

---
+++ src/difflib/IDifferentialEntityAnalyzer.java
@@ -0,0 +1,20 @@
+package difflib;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+
+/**
+ * Author : Yordanos Desta, on 5/10/17.
+ */
+public interface IDifferentialEntityAnalyzer {
+
+    HashMap<Field, Object> runDifferential();
+
+    HashMap<Field, Object> getDifferenceValues();
+
+    void setDifferentiableLevel(DifferentiableLevel differentiableLevel);
+
+    boolean hasDifference();
+
+    String getPrettyJson();
+}
