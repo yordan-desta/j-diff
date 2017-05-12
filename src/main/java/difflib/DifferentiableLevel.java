0097c9a04d9f35197b4a24ef1fbc390d5018bd29
Yordanos Desta <yordan.dev@gmail.com>
2017-05-12 15:54:05 +0300

    fixed documentation error

--- src/main/java/difflib/DifferentiableLevel.java
+++ src/main/java/difflib/DifferentiableLevel.java
@@ -20,17 +20,15 @@ public enum DifferentiableLevel {
      * what SHALLOW_UPDATE does plus it will give the diff for the fields of object fields. For example; if <br>
      * <code>class A{ <br>
      * private int someInt; <br>
-     * <p>
+     *
      * public void setSomeInt(int someInt){
-     * <p>
      * } <br>
      * } <br>
      * Class B{ <br>
      * private A aInstance; <br>
      * }<br>
-     * <p>
+     * 
      * and we have two instances of B- b1 and b2; DEEP flag will check the difference in the aInstance fields between the two instances of B and returns the modified values in those aInstance fields of B;
-     * <p>
      * </code>
      */
     DEEP;
